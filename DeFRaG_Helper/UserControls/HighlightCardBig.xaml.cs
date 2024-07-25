5cbc2c0724195663df98e43ca522d996e64e9054
netquick <netquick@gmx.ch>
2024-07-25 22:22:28 +0200

    highlight card rework

---
+++ DeFRaG_Helper/UserControls/HighlightCardBig.xaml.cs
@@ -0,0 +1,107 @@
+﻿using DeFRaG_Helper.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeFRaG_Helper.UserControls
+{
+    /// <summary>
+    /// Interaction logic for HighlightCardBig.xaml
+    /// </summary>
+    public partial class HighlightCardBig : UserControl
+    {
+        public HighlightCardBig()
+        {
+            InitializeComponent();
+        }
+
+
+        public static readonly DependencyProperty MapProperty = DependencyProperty.Register(
+       "Map", typeof(Map), typeof(HighlightCardBig), new PropertyMetadata(null, OnMapPropertyChanged));
+
+        public Map Map
+        {
+            get { return (Map)GetValue(MapProperty); }
+            set { SetValue(MapProperty, value); }
+        }
+
+        private static void OnMapPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as HighlightCardBig;
+            if (control != null)
+            {
+                control.DataContext = e.NewValue;
+            }
+        }
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                // Prevent event from bubbling up to parent elements
+                e.Handled = true;
+
+                // Call your connection logic here
+                MapViewModel.GetInstanceAsync().Result.SelectedMap = Map;
+                PlayMap();
+
+            }
+
+        }
+        private void PlayMap()
+        {
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+            int physicsSetting = mainWindow.GetPhysicsSetting(); // method in MainWindow
+            System.Diagnostics.Process.Start(AppConfig.GameDirectoryPath + "\\oDFe.x64.exe", $"+set fs_game defrag +df_promode {physicsSetting} +map {System.IO.Path.GetFileNameWithoutExtension(Map.Mapname)}");
+
+        }
+        private async void FavoriteCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            var checkBox = sender as CheckBox;
+            var map = checkBox.DataContext as Map;
+            if (map != null)
+            {
+                map.IsFavorite = 1;
+                var mapViewModel = await MapViewModel.GetInstanceAsync();
+
+                await mapViewModel.UpdateFavoriteStateAsync(map);
+
+            }
+        }
+
+        private async void FavoriteCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            var checkBox = sender as CheckBox;
+            var map = checkBox.DataContext as Map;
+            if (map != null)
+            {
+                map.IsFavorite = 0;
+                var mapViewModel = await MapViewModel.GetInstanceAsync();
+
+                await mapViewModel.UpdateFavoriteStateAsync(map);
+            }
+        }
+
+        private void UserControl_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Change to a slightly lighter or darker background color on hover
+            MainBorder.Background = new SolidColorBrush(Color.FromRgb(62, 62, 62)); // Example color
+        }
+
+        private void UserControl_MouseLeave(object sender, MouseEventArgs e)
+        {
+            // Revert to the original background color
+            MainBorder.Background = new SolidColorBrush(Color.FromRgb(45, 45, 45)); // Example color
+        }
+    }
+}
---
+++ DeFRaG_Helper/UserControls/HighlightServerCardBig.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeFRaG_Helper.UserControls
+{
+    /// <summary>
+    /// Interaction logic for HighlightServerCardBig.xaml
+    /// </summary>
+    public partial class HighlightServerCardBig : UserControl
+    {
+        public HighlightServerCardBig()
+        {
+            InitializeComponent();
+
+        }
+
+        public static readonly DependencyProperty ServerProperty = DependencyProperty.Register(
+         "Server", typeof(ServerNode), typeof(HighlightServerCardBig), new PropertyMetadata(null, OnServerPropertyChanged));
+
+
+        public ServerNode Server
+        {
+            get { return (ServerNode)GetValue(ServerProperty); }
+            set { SetValue(ServerProperty, value); }
+        }
+        private static void OnServerPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as HighlightServerCardBig;
+            if (control != null)
+            {
+                control.DataContext = e.NewValue;
+            }
+        }
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                // Prevent event from bubbling up to parent elements
+                e.Handled = true;
+
+                // Call your connection logic here
+                ConnectToServer();
+            }
+        }
+
+        private void ConnectToServer()
+        {
+            //connect to quake server under the ip and port in the server cards corresponding textblocks
+
+            if (DataContext is ServerNode serverNode)
+            {
+                // Execute the command
+                System.Diagnostics.Process.Start(AppConfig.GameDirectoryPath + "\\oDFe.x64.exe", $"+connect {serverNode.IP}:{serverNode.Port}");
+
+            }
+
+
+            // Your logic to initiate a connection to the server
+            Debug.WriteLine($"Connecting to server at ");
+            // Implement the actual connection logic here
+        }
+        private void UserControl_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Change to a slightly lighter or darker background color on hover
+            MainBorder.Background = new SolidColorBrush(Color.FromRgb(62, 62, 62)); // Example color
+        }
+
+        private void UserControl_MouseLeave(object sender, MouseEventArgs e)
+        {
+            // Revert to the original background color
+            MainBorder.Background = new SolidColorBrush(Color.FromRgb(45, 45, 45)); // Example color
+        }
+    }
+}
